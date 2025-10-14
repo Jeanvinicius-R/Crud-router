efc0586e4adc33bfcc38a8185997b1e85a13e19c
Jeanvinicius.r <jvr.classroom@gmail.com>
2025-10-13 22:52:24 -0300

    Criamos as pastas, arquivos e funcções para o começo da atividade

--- crud-router/src/App.js
+++ crud-router/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routers,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+// importação das páginas
+import Login from "./pages/Login/Login";
+
+// importação do Navbar (menu principal)
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  //Estado global simples para login e contatos
+  const [isAutenticated, setIsAuthenticated] = useState(false);
+  const [contacts, setContacts] = useState([]);
 
+  // Função de login
+  const handleLogin = (username, password) => {
+    if (username === "admin" && password === "senha") {
+      setIsAuthenticated(true);
+    } else {
+      alert("Usuário ou senha inválidos");
+    }
+  };
+
+  //Função de logout
+  const handleLogout = () => {
+    setIsAuthenticated(false);
+  };
+}
 export default App;
---
+++ crud-router/src/pages/Login/Login.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./Login.css";
+
+function Login({ onLogin }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onLogin(username, password);
+  }
+
+  return (
+    <div>
+      <form onSubmit = {handleSubmit}>
+        <h2>Agenda de Contatos</h2>
+        <p> Faça login para continuar !</p>
+        <input
+          type="text"
+          placeholder="Digite o seu usuário"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Digite a sua senha (senha)"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
+  );
+}
+export default Login;
